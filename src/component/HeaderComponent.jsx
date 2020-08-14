2980f80b1d8760f7ec1834f92b5fede9d4444300
baejunsun <bjs265811@gmail.com>
2020-08-14 14:07:09 +0900

    useSelector가 왜 안될까요

--- src/component/HeaderComponent.jsx
+++ src/component/HeaderComponent.jsx
@@ -4,6 +4,7 @@ import "../styles/covidLive.svg";
 import Logo from "../styles/covidLive.jpg";
 import styles from "../styles/header.module.scss"
 export function HeaderComponent({nickName, logout}) {
+  console.log('컴포넌트', nickName)
   return (
     <header className={styles.header}>
       <span>
