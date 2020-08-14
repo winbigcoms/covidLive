1648abd16ba255e1d9ee2a326880c421ebc34d04
baejunsun <bjs265811@gmail.com>
2020-08-14 17:19:05 +0900

    email check

--- src/component/SigninComponent.jsx
+++ src/component/SigninComponent.jsx
@@ -2,24 +2,26 @@ import React from 'react'
 import styles from '../styles/signIn.module.scss'
 
 export default function SigninComponent({login}) {
-  const emailRef = React.useRef()
+  // const [state, setState] = useState({email:''})
+
+  const buttonRef = React.useRef();
+  const emailRef = React.useRef();
   const passwordRef = React.useRef()
 
   return (
     <div className={styles.sign_body}>
     <form>
         <h1 className={styles.h1}>Wellcome<p className={styles.letter}>Corona Service</p></h1>
-        <div className={styles.bar}></div>
         <div className={styles.sign_id}>
           <p>Email</p>
-          <input className={styles.sign_input} type="email" ref={emailRef} />
+          <input className={styles.sign_input} type="email" onChange={change} ref={emailRef} placeholder="abc@gmail.com"/>
         </div>
         <div className={styles.sign_pw}>
           <p>Password</p>
           <input className={styles.sign_input} type="password" ref={passwordRef} />
         </div>
         <div>
-          <button className={styles.sign_btn} type="primary" onClick={click}>Sign-in</button>
+          <button className={styles.sign_btn} type="primary" ref={buttonRef} onClick={click}>Sign-in</button>
         </div>
       </form>
       </div>
@@ -27,8 +29,19 @@ export default function SigninComponent({login}) {
    
   function click () {
     const email = emailRef.current.value;
+    console.log(email)
     const password = passwordRef.current.value;
     login(email, password)
   }
 
+  function change (e) {    
+    const email = e.target.value
+    const exptext = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+      if(exptext.test(email)===false){
+          buttonRef.current.disabled = true
+      }else {
+      buttonRef.current.disabled = false
+      }
+    };
 }
