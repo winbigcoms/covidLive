d960bdd859ca259ae0bc1f548e1196b1d6442d62
winbigcoms <bigcoms6290@gmail.com>
2020-10-15 17:41:46 +0900

    Feat calc time

--- src/services/CovidService.js
+++ src/services/CovidService.js
@@ -4,10 +4,12 @@ export default class CovidService{
   static async getCityData(){
     const cityKey = "zD%2B5S1HIVi5RfD%2FvZ0AKwZduqAYQAjJ%2B%2FlQnIsVCYmaRmd0F%2BtMEjmvtU3NrhxcSdkUuzkhPZynYNKyM3Ya6Aw%3D%3D";
     const date = new Date();
-    const getDataDate = `${date.getFullYear()}0${date.getMonth()+1}${date.getDate()}`;
+    const getDataDate = `${date.getFullYear()}${date.getMonth()+1 < 10 ? "0"+date.getMonth()+1:date.getMonth()+1}${date.getDate()}`;
+    console.log(`/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=${cityKey}&startCreateDt=${getDataDate}&endCreateDt=${getDataDate}`)
     const data = await Axios.get(`/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=${cityKey}&startCreateDt=${getDataDate}&endCreateDt=${getDataDate}`);
     // 지역 데이터 총 17개, 17번이 총계
     const needData = data.data.response.body.items.item;
+    console.log(needData)
     needData.shift();
     return needData
   }
