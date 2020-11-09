6273b9cc78cbea4dcbe5ed99361ab8057641ed44
winbigcoms <bigcoms6290@gmail.com>
2020-11-09 18:14:57 +0900

    Updates

---
+++ precache-manifest.371803f2cd54fc1c1efc853a5689b957.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e05fab632ec49dc4c8738b005a4ba2",
+    "url": "/covidLive/index.html"
+  },
+  {
+    "revision": "f9ceb529a8d127089a53",
+    "url": "/covidLive/static/css/main.b8e3e686.chunk.css"
+  },
+  {
+    "revision": "227b0fc1e3ad895a33b8",
+    "url": "/covidLive/static/js/2.da1e2368.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/covidLive/static/js/2.da1e2368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ceb529a8d127089a53",
+    "url": "/covidLive/static/js/main.e4968e4b.chunk.js"
+  },
+  {
+    "revision": "2ac037e627fef2473105",
+    "url": "/covidLive/static/js/runtime-main.e0682996.js"
+  },
+  {
+    "revision": "e51852e4518f5a0818de08102b5b38e0",
+    "url": "/covidLive/static/media/covidLive.e51852e4.jpg"
+  },
+  {
+    "revision": "86a36f22efa917d3bee78ac71b3ae565",
+    "url": "/covidLive/static/media/signinBackground.86a36f22.jpg"
+  },
+  {
+    "revision": "3711f0a3167098781ee33132ebd40dec",
+    "url": "/covidLive/static/media/whereRUComing.3711f0a3.jpg"
+  }
+]);
